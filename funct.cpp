36016678ef9617f030bc3a94a55cbd86a8941778
fadd00 <andhikahutama9@gmail.com>
2024-05-07 15:56:24 +0700

    calss mahasiswa

--- funct.cpp
+++ funct.cpp
@@ -2,6 +2,15 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+    private:
+    string nama;
+
+    public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+
 int main(){
-    
+
 }
