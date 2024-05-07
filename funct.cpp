cc5d32c54752301546b2b7862f8ad5e0392ab28b
fadd00 <andhikahutama9@gmail.com>
2024-05-07 15:57:50 +0700

    membuat void setNama

--- funct.cpp
+++ funct.cpp
@@ -5,12 +5,15 @@ using namespace std;
 class mahasiswa{
     private:
     string nama;
-
     public:
     friend void setNama(mahasiswa &a, string);
 };
-
-
+void setNama(mahasiswa &a, string b){
+    a.nama = b;
+    cout << a.nama;
+}
 int main(){
-
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
 }
